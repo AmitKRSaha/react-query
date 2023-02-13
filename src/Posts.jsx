84ba055f21e344a62e437e2eb8bb89ac9127558b
amit2590 <amit.kumar.saha.consultant@heart.org>
2023-02-13 20:37:21 +0530

    first commit

---
+++ src/Posts.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from 'react-query'
+
+import { PostDetail } from "./PostDetail";
+
+
+const maxPostPage = 10;
+
+async function fetchPosts(pageNumber) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // For prefetching next  thats curr + 1 request data
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if(currentPage < maxPostPage){
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(
+        ['posts', nextPage],
+        () => fetchPosts(nextPage),
+      )
+    }
+    
+  }, [currentPage, queryClient]);
+
+
+  // replace with useQuery
+  const { data, isError, error, isLoading } = useQuery(
+    ['posts', currentPage], // Currentpage now is dependent if it changes it gonna refetch
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true, // For holding data in cache
+    });
+
+  if (isLoading) return <div>Loading</div>;
+  if (isError) {
+    return (
+      <div>
+        <div>Something went wrong</div>
+        <p> {error.toString()} </p>
+      </div>
+    )
+  }
+
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled ={ currentPage <= 1}
+        onClick={() => {
+          setCurrentPage((prevVal) => prevVal -1  )
+         }}>
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button disabled ={ currentPage >= maxPostPage} onClick={() => {
+          setCurrentPage((prevVal) => prevVal +1  )
+
+         }}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
